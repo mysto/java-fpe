f4445dbbac7e725f7fdba39d8b91c7143131020c
Brad Schoening <5796692+bschoening@users.noreply.github.com>
2021-11-02 22:12:15 -0400

    added jar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,7 @@ dependencies {
     testImplementation("junit", "junit", "4.12")
     implementation("org.apache.logging.log4j:log4j-api:2.1")
 }
+
+tasks.jar {
+    archiveFileName.set("ff3.jar")
+}
