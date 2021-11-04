32ae34b0605daf20181f57e7e579f3d9fd4159c2
Brad Schoening <5796692+bschoening@users.noreply.github.com>
2021-11-04 00:36:10 -0400

    added publish section

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     java
     // id("me.champeau.jmh") version "0.6.6"
+    `maven-publish`
+    signing
 }
 
-group = "com.privacylogistics"
-version = "1.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
@@ -16,6 +16,74 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.1")
 }
 
-tasks.jar {
-    archiveFileName.set("ff3.jar")
+group = "io.github.mysto"
+version = "0.9"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "io.github.mysto"
+            artifactId = "ff3"
+            version = "0.9"
+
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("ff3")
+                description.set("A Format-preserving encryption library for FF3-1")
+                url.set("http://privacylogistics.com")
+                properties.set(mapOf(
+                    "myProp" to "value",
+                    "prop.with.dots" to "anotherValue"
+                ))
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("bschoeni")
+                        name.set("Brad Schoening")
+                        //email.set("bschoeni@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/mysto/java-fpe.git")
+                    developerConnection.set("scm:git:ssh://github.com/mysto/java-fpe.git")
+                    url.set("http://github.com/mysto/java-fpe/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri(layout.buildDirectory.dir("repos"))
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
