6059264ba3901612b139e9dd95144c973c3045c3
Brad Schoening <5796692+bschoening@users.noreply.github.com>
2021-12-03 19:38:02 -0500

    added test task

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
     implementation("org.apache.logging.log4j:log4j-api:2.1")
 }
+
 
 group = "io.github.mysto"
 version = "0.9"
@@ -23,6 +24,10 @@ version = "0.9"
 java {
     withJavadocJar()
     withSourcesJar()
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
 
 publishing {
