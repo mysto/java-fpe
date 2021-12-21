75b427bd0c96ad4a9f180590b1498c57581a16ae
Brad Schoening <5796692+bschoening@users.noreply.github.com>
2021-12-20 22:41:39 -0500

    enable UTF-8 for unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,10 @@ tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
+tasks.withType<Test> {
+    systemProperty("file.encoding", "UTF-8")
+}
+
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
