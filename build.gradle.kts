a3ff79c6caa96b1139fc11abdd58e3fb7eec891c
Brad Schoening <5796692+bschoening@users.noreply.github.com>
2021-12-30 22:19:52 -0500

    updated Log4J version to 2.17.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
-    implementation("org.apache.logging.log4j:log4j-api:2.16.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.1")
 }
 
 
