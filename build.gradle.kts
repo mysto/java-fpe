7d12b600bf335beac37ca6ec6934e4408c919ba1
Brad Schoening <bschoeni@gmail.com>
2024-11-18 17:53:44 -0500

    Update junit to 5.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
     implementation("org.apache.logging.log4j:log4j-api:2.17.1")
     implementation("org.apache.logging.log4j:log4j-core:2.17.1")
 }
